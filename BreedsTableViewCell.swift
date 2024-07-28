418d900cdf34a16237f56b9517c05d2c924a65b4
bukreev <bukreev.e.a@icloud.com>
2024-07-28 22:41:12 +0300

    Simple demo with mock data

---
+++ BreedsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BreedsTableViewCell.swift
+//  DemoApp
+//
+//  Created by Eugene on 28.07.2024.
+//
+
+import UIKit
+
+class BreedsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CatBreeds.swift
@@ -0,0 +1,8 @@
+//
+//  CatBreeds.swift
+//  DemoApp
+//
+//  Created by Eugene on 28.07.2024.
+//
+
+import Foundation
